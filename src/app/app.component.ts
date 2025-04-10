2ccfb21a4929825af9d31fee012d7223df0b6181
Kinarov Kristian <kinarov.k@gmail.com>
2025-04-10 16:43:23 +0200

    added task

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from "@angular/core";
 import {
   combineLatest,
+  debounceTime,
   filter,
   forkJoin,
   map,
@@ -8,13 +9,13 @@ import {
   Subject,
   Subscription,
   switchMap,
-} from 'rxjs';
-import { MockDataService } from './mock-data.service';
+} from "rxjs";
+import { MockDataService } from "./mock-data.service";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
 export class AppComponent implements OnInit, OnDestroy {
   searchTermByCharacters = new Subject<string>();
@@ -34,7 +35,7 @@ export class AppComponent implements OnInit, OnDestroy {
     // 1.1. Add functionality to changeCharactersInput method. Changes searchTermByCharacters Subject value on input change.
     const inputValue: string = element.target.value;
     // YOUR CODE STARTS HERE
-
+    this.searchTermByCharacters.next(inputValue);
     // YOUR CODE ENDS HERE
   }
 
@@ -45,34 +46,61 @@ export class AppComponent implements OnInit, OnDestroy {
 
     // 3. Add debounce to prevent API calls until user stop typing.
 
-    this.charactersResults$ = this.searchTermByCharacters
-        .pipe
-        // YOUR CODE STARTS HERE
+    this.charactersResults$ = this.searchTermByCharacters.pipe(
+      // YOUR CODE STARTS HERE
 
-        // YOUR CODE ENDS HERE
-        ();
+      debounceTime(300),
+      filter((searchTerm: string) => searchTerm.length >= 3),
+      switchMap((seachterm: string) =>
+        this.mockDataService.getCharacters(seachterm)
+      )
+    );
+    // YOUR CODE ENDS HERE
   }
 
   loadCharactersAndPlanet(): void {
     // 4. On clicking the button 'Load Characters And Planets', it is necessary to process two requests and combine the results of both requests into one result array. As a result, a list with the names of the characters and the names of the planets is displayed on the screen.
     // Your code should looks like this: this.planetAndCharactersResults$ = /* Your code */
     // YOUR CODE STARTS HERE
+    this.planetAndCharactersResults$ = forkJoin([
+      this.mockDataService.getCharacters(""),
+      this.mockDataService.getPlanets(),
+    ]).pipe(
+      map(([characters, planets]) => {
+        const characterNames = characters.map((c: any) => c.name);
+        const planetNames = planets.map((p: any) => p.name);
+        return [
+          ...characterNames.map((name: any) => ({ name })),
+          ...planetNames.map((name: any) => ({ name })),
+        ];
+      })
+    );
+    console.log("hmmm");
     // YOUR CODE ENDS HERE
   }
 
   initLoadingState(): void {
     /* 5.1. Let's add loader logic to our page. For each request, we have an observable that contains the state of the request. When we send a request the value is true, when the request is completed, the value becomes false. You can get value data with mockDataService.getCharactersLoader() and mockDataService.getPlanetLoader().
-
+    
     - Combine the value of each of the streams.
     - Subscribe to changes
     - Check the received value using the areAllValuesTrue function and pass them to the isLoading variable. */
     // YOUR CODE STARTS HERE
+    const charactersLoader = this.mockDataService.getCharactersLoader();
+    const planetsLoader = this.mockDataService.getPlanetLoader();
+
+    combineLatest([charactersLoader, planetsLoader]).subscribe(
+      (loaderStatus: boolean[]) => {
+        this.isLoading = this.areAllValuesTrue(loaderStatus);
+      }
+    );
     // YOUR CODE ENDS HERE
   }
 
   ngOnDestroy(): void {
     // 5.2 Unsubscribe from all subscriptions
     // YOUR CODE STARTS HERE
+    this.subscriptions.forEach((sub) => sub.unsubscribe());
     // YOUR CODE ENDS HERE
   }
 
